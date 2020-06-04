76199a6cec7e50335c7913ae97aa1ae7eb937e3a
Anthony <anthony.trillo.92@gmail.com>
2020-06-04 14:48:50 -0300

    Corrigiendo detalles de estilos

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ app.use(express.json());
 
 app.use(require('./routes/index'));
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-    res.sendfile(__dirname + '/public/index.html');
-});
+// app.get('/', function(req, res){
+//     res.sendfile(__dirname + '/public/index.html');
+// });
 
 app.listen(3000, () => {
     console.log('Server on port 3000');
